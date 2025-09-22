0532c4079167539972f9370695dd5c70006c66b6
filipmarecek03 <filip.marecek03@gmail.com>
2025-09-22 12:57:11 +0200

    kod2209

---
+++ Začatky/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Začatky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   string slovo1;
+               string slovo2;
+               Console.WriteLine("Zadej 1. slovo");
+               slovo1 = Console.ReadLine();
+               Console.WriteLine("Zadej 2. slovo");
+               slovo2 = Console.ReadLine();
+               Console.WriteLine("slovo 1 bylo: " + slovo1 + " slovo 2 bylo: " + slovo2);
+            */
+            /*
+            int a;
+            int b;
+            int vysledek;
+            Console.WriteLine("Zadej první hodnotu");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zdej druhou hodnotu");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zdej druhou hodnotu");
+            vysledek = a + b;
+            Console.WriteLine("¨Vysledek souctu je: " + vysledek);
+            */
+
+        }
+    }
+}
---
+++ ahoj  světe/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ahoj__světe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej své jméno:");
+            string uzivatel = Console.ReadLine();
+            Console.WriteLine("Ahoj " + uzivatel);
+        }
+    }
+}
